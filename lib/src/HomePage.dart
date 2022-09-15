eb0f287bd8d3d1287b797ba00d36104acf2d4a68
tranphilong2406 <tranphilong2406>
2022-09-15 17:58:04 +0700

    delete images folder - test some feature

--- lib/src/HomePage.dart
+++ lib/src/HomePage.dart
@@ -8,8 +8,34 @@ class HomePage extends StatelessWidget {
         child: CupertinoButton(
             child: Text('Tap me!!'),
             color: CupertinoColors.activeOrange,
-            onPressed: () {}),
+            onPressed: () {
+              showDialog(context);
+            }),
       ),
     );
+  }
+
+  void showDialog(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+              title: const Text('This is an Alert'),
+              actions: [
+                CupertinoDialogAction(
+                  child: const Text('Nope'),
+                  isDestructiveAction: true,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                CupertinoDialogAction(
+                  child: const Text('Hello'),
+                  isDefaultAction: true,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ));
   }
 }
